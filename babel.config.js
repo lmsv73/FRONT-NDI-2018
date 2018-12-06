3c532688f00238a8bb3e728b03602d8e5e5d436f
Ludovic Millon <developer@netdev.fr>
2018-12-06 20:59:14 +0100

    Lancement

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/assets/sass/global.scss")]
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import router from './router.js'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
