423f5d33a75d85d80ac8397bab13c0bad8553b15
Ludovic Millon <developer@netdev.fr>
2018-12-06 22:20:07 +0100

    blablablz

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
+  <div class="home h-100">
     <navbar></navbar>
+    <b-container fluid class="home-container">
+      <b-row class="h-100">
+        <b-col class="pt-3 home-left-side" md="3">
+          <div>
+            <div class="weather-header">Weather</div>
+          </div>
+        </b-col>
+        <b-col md="6"></b-col>
+        <b-col md="3"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
@@ -13,20 +25,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
