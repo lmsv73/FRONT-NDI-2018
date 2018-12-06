b1bfdc31cd76b4ef8cc9b9f6b5f96cf31a82a576
TrimA <florianseb96@hotmail.fr>
2018-12-06 21:26:50 +0100

    ✨ added missions + stock pages

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './components/Home'
+import Stock from './components/Stock'
+import Missions from './components/Missions'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes: [
-      { path: '/', component: Home }
+      { path: '/', component: Home },
+      { path: '/stock', component: Stock },
+      { path: '/missions', component: Missions }
     ]
 })
 
