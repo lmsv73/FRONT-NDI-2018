2946894955d3b7dc25b7b055dfd4e8a637a55c27
TrimA <florianseb96@hotmail.fr>
2018-12-06 21:33:24 +0100

    ✨ added basic table for missions

--- src/components/Missions.vue
+++ src/components/Missions.vue
@@ -1,8 +1,23 @@
 <template>
-    <h1>Missions</h1>
+<div>
+  <h1>Missions</h1>
+  <b-table striped hover :items="items"></b-table>
+</div>
+
 </template>
 <script>
+const items = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+  { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+]
 export default {
-    name : 'missions'
+    name : 'missions',
+    data () {
+    return {
+      items: items
+    }
+  }
 }
 </script>
