e61d8b9c28d453bc80ef54332d745214ce6047ad
Ludovic Millon <developer@netdev.fr>
2018-12-06 21:16:38 +0100

    On repare les conneries

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  pluginOptions: {
-    "style-resources-loader": {
-      preProcessor: "scss",
-      patterns: [path.resolve(__dirname, "./src/assets/sass/global.scss")]
-    }
-  }
+  ]
 }
