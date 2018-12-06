a333b3b08a9514b94a5388110cc4667aa7ebbf1c
TrimA <florianseb96@hotmail.fr>
2018-12-06 22:10:43 +0100

    ✨ added journal

---
+++ src/components/Journal.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <h1 class="m-4">Journal</h1>
+    </div>
+</template>
+<script>
+export default {
+    name : 'journal',
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from './components/Home'
 import Stock from './components/Stock'
 import Missions from './components/Missions'
+import Journal from './components/Journal'
 
 Vue.use(VueRouter)
 
@@ -10,7 +11,8 @@ const router = new VueRouter({
     routes: [
       { path: '/', component: Home },
       { path: '/stock', component: Stock },
-      { path: '/missions', component: Missions }
+      { path: '/missions', component: Missions },
+      { path: '/journal', component: Journal }
     ]
 })
 
